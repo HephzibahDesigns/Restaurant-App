fca9c2a63ba83fcb6dd883e46166f96e91dc5746
Paul Boluwatife <paulboluwatife377@gmail.com>
2023-05-10 09:00:11 +0100

    final features added

--- resturant-app/src/components/Navbar/Navbar.jsx
+++ resturant-app/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import logo from "../../Asset/gericht.png";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [activeTab, setActiveTab] = useState("home");
   return (
     <div className="app__navbar">
       <div className="app__navbar-logo">
@@ -14,23 +15,53 @@ const Navbar = () => {
 
       <ul className="app__navbar-links">
         <li className="p__opensans">
-          <a href="#home">Home</a>
+          <a
+            className={activeTab === "home" ? "active" : " "}
+            onClick={() => setActiveTab("home")}
+            href="#home"
+          >
+            Home
+          </a>
         </li>
 
         <li className="p__opensans">
-          <a href="#about">About</a>
+          <a
+            className={activeTab === "about" ? "active" : " "}
+            onClick={() => setActiveTab("about")}
+            href="#about"
+          >
+            About
+          </a>
         </li>
 
         <li className="p__opensans">
-          <a href="#menu">Menu</a>
+          <a
+            className={activeTab === "menu" ? "active" : " "}
+            onClick={() => setActiveTab("menu")}
+            href="#menu"
+          >
+            Menu
+          </a>
         </li>
 
         <li className="p__opensans">
-          <a href="#awards">Awards</a>
+          <a
+            className={activeTab === "awards" ? "active" : " "}
+            onClick={() => setActiveTab("awards")}
+            href="#awards"
+          >
+            Awards
+          </a>
         </li>
 
         <li className="p__opensans">
-          <a href="#contact">Contact</a>
+          <a
+            className={activeTab === "contact" ? "active" : " "}
+            onClick={() => setActiveTab("contact")}
+            href="#contact"
+          >
+            Contact
+          </a>
         </li>
       </ul>
 
@@ -67,23 +98,53 @@ const Navbar = () => {
             />
             <ul className="app__navbar-smallsreen-links">
               <li className="p__opensans">
-                <a href="#home">Home</a>
+                <a
+                  className={activeTab === "home" ? "active2" : " "}
+                  onClick={() => setActiveTab("home")}
+                  href="#home"
+                >
+                  Home
+                </a>
               </li>
 
               <li className="p__opensans">
-                <a href="#about">About</a>
+                <a
+                  className={activeTab === "about" ? "active2" : " "}
+                  onClick={() => setActiveTab("about")}
+                  href="#about"
+                >
+                  About
+                </a>
               </li>
 
               <li className="p__opensans">
-                <a href="#menu">Menu</a>
+                <a
+                  className={activeTab === "menu" ? "active2" : " "}
+                  onClick={() => setActiveTab("menu")}
+                  href="#menu"
+                >
+                  Menu
+                </a>
               </li>
 
               <li className="p__opensans">
-                <a href="#awards">Awards</a>
+                <a
+                  className={activeTab === "awards" ? "active2" : " "}
+                  onClick={() => setActiveTab("awards")}
+                  href="#awards"
+                >
+                  Awards
+                </a>
               </li>
 
               <li className="p__opensans">
-                <a href="#contact">Contact</a>
+                <a
+                  className={activeTab === "contact" ? "active2" : " "}
+                  onClick={() => setActiveTab("contact")}
+                  href="#contact"
+                >
+                  Contact
+                </a>
               </li>
             </ul>
           </div>
